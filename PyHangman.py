ae581f9bc7d7b3f69ee7c01ce9311a4f28ab97b0
JeongbeenSon <jeongbeenson19@gmail.com>
2023-11-28 23:39:12 +0900

    231128

---
+++ PyHangman.py
@@ -0,0 +1,7 @@
+import random
+
+words_list = ["apple", "angel", "balloon", "block" , "chicken", "chalk", "disguise", "drill", "economy", "error" "football", "fund",
+               "ground", "grill", "hypnotize", "hyundai", "ironic", "irreplaceable", "judge", "jungle", "king", "korea",
+               "lemon", "league", "money", "mother", "need", "neither", "occur", "obeject", "power", "pineapple", "queen",
+               "quit", "reunited", "region" "secret", "sharp", "tour", "type", "useless", "unnecessary", "victory", "victim",
+               "wound", "world", "xylophone", "xray", "zebra", "zero"]
