0d8c3cc8cd77f55dd63a3c8a464942d990f7631e
JeongbeenSon <jeongbeenson19@gmail.com>
2023-12-03 23:42:42 +0900

    231203

--- GRADING PROGRAM.py
+++ GRADING PROGRAM.py
@@ -7,7 +7,7 @@ student_scores = {
 }
 # 🚨 Don't change the code above 👆
 # TODO-1: Create an empty dictionary called student_grades.
-
+student_grades = ""
 
 # TODO-2: Write your code below to add the grades to student_grades.👇
 
