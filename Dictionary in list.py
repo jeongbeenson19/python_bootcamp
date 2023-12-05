08308919c6dc3d6e355d8cd3ee1846634d4b9ea9
JeongbeenSon <jeongbeenson19@gmail.com>
2023-12-05 17:26:19 +0900

    231205

---
+++ Dictionary in list.py
@@ -0,0 +1,32 @@
+country = input() # Add country name
+visits = int(input()) # Number of visits
+list_of_cities = eval(input()) # create list from formatted string
+
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+# Do NOT change the code above 👆
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 
+def add_new_country(a, b, c):
+  new_log = {}
+  new_log["country"] = str(a)
+  new_log["visits"] = b
+  new_log["cities"] = c
+
+  travel_log.append(new_log)
+  
+# Do not change the code below 👇
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
