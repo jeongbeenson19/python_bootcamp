ac7aa038de26c9540d6833ffbb107cec8239022f
Jeongbeen_Son <jeongbeenson19@gmail.com>
2023-12-11 22:43:27 +0900

    231211

---
+++ Higher-lower-football/HigherLower.py
@@ -0,0 +1,59 @@
+from random import randint
+import data
+import art
+import os
+
+player_profile = data.data
+game_end = False
+score = 0
+number = 14
+
+def clear():
+    os.system('cls')
+    
+#난수 생성
+def make_comparsion():
+    return player_profile[randint(0, len(player_profile) - 1)]
+
+#문제 출력
+first_comparison = make_comparsion()
+
+def HigherLower(first_comparison, score, game_end):
+    if game_end == True:
+        return
+    clear()
+    print(art.logo)
+    print(f"{first_comparison['name']} who play for {first_comparison['team']}")
+    print(art.vs)
+    comparison = make_comparsion()
+
+    while comparison['value'] == first_comparison['value']:
+        comparison = make_comparsion()
+
+    print(f"{comparison['name']} who play for {comparison['team']}")
+    choice = input("Type '1' to choice first one, Type '2' to choice second one: ")
+    
+    if (choice == '1' and float(first_comparison['value']) > float(comparison['value'])) or (choice == '2' and float(comparison['value']) > float(first_comparison['value'])):
+        score += 1
+    else:
+        print(f"you are wrong, you are final score is {score}")
+        game_end = True
+    
+    first_comparison = comparison
+
+    return first_comparison, score, game_end
+
+for n in range(0, number):
+    first_comparison, score, game_end = HigherLower(first_comparison, score, game_end)
+
+    if game_end == True:
+        break
+
+
+    
+
+#정답
+
+#채점
+
+#처리
---
+++ Higher-lower-football/data.py
@@ -0,0 +1,467 @@
+data = [
+    {
+        'name': 'Erling Haaland',
+        'value': '180',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Bukayo Saka',
+        'value': '120',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'Phil Foden',
+        'value': '110',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Rodri',
+        'value': '100',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Declan Rice',
+        'value': '100',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'Moises Caiscedo',
+        'value': '90',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Martin Ødegaard',
+        'value': '90',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'Bruno Guimaraes',
+        'value': '85',
+        'team': 'Newcastle',
+    },
+    {
+        'name': 'Josko Gvardiol',
+        'value': '80',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Julian Alvarez',
+        'value': '80',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Christopher Nkunku',
+        'value': '80',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Gabriel Martinelli',
+        'value': '80',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'Bernardo Silva',
+        'value': '80',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Enzo Fernandez',
+        'value': '80',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Ruben Dias',
+        'value': '80',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Jack Grealish',
+        'value': '75',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Marcus Rashford',
+        'value': '75',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'Gabriel Jesus',
+        'value': '75',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'Luis Diaz',
+        'value': '75',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'William Saliba',
+        'value': '70',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'James Maddison',
+        'value': '70',
+        'team': 'Tottenham',
+    },
+    {
+        'name': 'Dominik Szoboszlai',
+        'value': '70',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Kevin De Bruyne',
+        'value': '70',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Alexander Isak',
+        'value': '70',
+        'team': 'Newcastle',
+    },
+    {
+        'name': 'Lucas Paqueta',
+        'value': '65',
+        'team': 'West Ham Utd',
+    },
+    {
+        'name': 'Evan Ferguson',
+        'value': '65',
+        'team': 'Brighton',
+    },
+    {
+        'name': 'Mohamed Salah',
+        'value': '65',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Alexis Mac Allister',
+        'value': '65',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Kai Havertz',
+        'value': '60',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'Mason Mount',
+        'value': '60',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'Cristian Romero',
+        'value': '60',
+        'team': 'Tottenham',
+    },
+    {
+        'name': 'Reece James',
+        'value': '55',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Joao Palhinha',
+        'value': '55',
+        'team': 'Fullham',
+    },
+    {
+        'name': 'Douglas Luiz',
+        'value': '55',
+        'team': 'Aston Villa',
+    },
+    {
+        'name': 'Cody Gakpo',
+        'value': '55',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Ollie Watkins',
+        'value': '55',
+        'team': 'Aston Villa',
+    },
+    {
+        'name': 'Antony',
+        'value': '50',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'Diogo Jota',
+        'value': '50',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Kaoru Mitoma',
+        'value': '50',
+        'team': 'Brighton',
+    },
+    {
+        'name': 'Heung-min Son',
+        'value': '50',
+        'team': 'Tottenham',
+    },
+    {
+        'name': 'Jarrod Bowen',
+        'value': '48',
+        'team': 'West Ham Utd',
+    },
+    {
+        'name': 'Brennan Jhonson',
+        'value': '48',
+        'team': 'Tottenham',
+    },
+    {
+        'name': 'Mykhaylo Mudryk',
+        'value': '45',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Anthony Gordon',
+        'value': '45',
+        'team': 'Newcastle',
+    },
+    {
+        'name': 'Nathan Ake',
+        'value': '42',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Luke Shaw',
+        'value': '42',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'Levi Colwill',
+        'value': '42',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Ederson',
+        'value': '40',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Pedro Porro',
+        'value': '40',
+        'team': 'Tottenham',
+    },
+    {
+        'name': 'Morgan Gibbs-White',
+        'value': '40',
+        'team': 'Nottingham Forest',
+    },
+    {
+        'name': 'Richarlison',
+        'value': '40',
+        'team': 'Tottenham',
+    },
+    {
+        'name': 'Andrew Robertson',
+        'value': '40',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Andre Onana',
+        'value': '40',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'John Stones',
+        'value': '40',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Ibrahima Konate',
+        'value': '38',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Virgil van Dijk',
+        'value': '35',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Ben Chilwell',
+        'value': '35',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Ryan Gravenberch',
+        'value': '35',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Wataru Endo',
+        'value': '13',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Alisson',
+        'value': '35',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Trent Alexander-Arnold',
+        'value': '65',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Darwin Nunez',
+        'value': '65',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Thiago',
+        'value': '12',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Harry Maguire',
+        'value': '20',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'Raphael Varane',
+        'value': '35',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'Scott McTominay',
+        'value': '25',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'Anthony Martial',
+        'value': '15',
+        'team': 'Manchester United',
+    },
+    {
+        'name': 'Kyle Walker',
+        'value': '13',
+        'team': 'Manchester City',
+    },
+    {
+        'name': 'Thiago Silva',
+        'value': '2',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Roberto Sanchez',
+        'value': '28',
+        'team': 'Chelsea',
+    },
+    {
+        'name': 'Takehiro Tomiyasu',
+        'value': '25',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'Aaron Ramsdale',
+        'value': '32',
+        'team': 'Arsenal',
+    },
+    {
+        'name': 'Tariq Lamptey',
+        'value': '12',
+        'team': 'Brighton',
+    },
+    {
+        'name': 'Adam Lallana',
+        'value': '1.2',
+        'team': 'Brighton',
+    },
+    {
+        'name': 'Jose Sa',
+        'value': '18',
+        'team': 'Wolves',
+    },
+    {
+        'name': 'Hee-chan Hwang',
+        'value': '18',
+        'team': 'Wolves',
+    },
+    {
+        'name': 'Pedro Neto',
+        'value': '32',
+        'team': 'Wolves',
+    },
+    {
+        'name': 'Kylian Mbappe',
+        'value': '180',
+        'team': 'PSG',
+    },
+    {
+        'name': 'Jude Bellingham',
+        'value': '150',
+        'team': 'Real Madrid',
+    },
+    {
+        'name': 'Victor Osimhen',
+        'value': '120',
+        'team': 'Napoli',
+    },
+    {
+        'name': 'Harry Kane',
+        'value': '110',
+        'team': 'Bayern',
+    },
+    {
+        'name': 'Khvicha Kvaratskelia',
+        'value': '85',
+        'team': 'Napoli',
+    },
+    {
+        'name': 'Joshua Kimmich',
+        'value': '75',
+        'team': 'Bayern',
+    },
+    {
+        'name': 'Alphonso Davies',
+        'value': '70',
+        'team': 'Bayern',
+    },
+    {
+        'name': 'Ousmane Dembele',
+        'value': '60',
+        'team': 'PSG',
+    },
+    {
+        'name': 'Neymar',
+        'value': '50',
+        'team': 'Al-Hilal',
+    },
+    {
+        'name': 'Federico Chiesa',
+        'value': '45',
+        'team': 'Juventus',
+    },
+    {
+        'name': 'Gianluigi Donnarumma',
+        'value': '45',
+        'team': 'PSG',
+    },
+    {
+        'name': 'Fabinho',
+        'value': '38',
+        'team': 'Liverpool',
+    },
+    {
+        'name': 'Min-jae Kim',
+        'value': '60',
+        'team': 'Bayern',
+    },
+    {
+        'name': 'Kang-in Lee',
+        'value': '22',
+        'team': 'PSG',
+    },
+    {
+        'name': 'Takefusa Kubo',
+        'value': '50',
+        'team': 'Real Sociedad',
+    },
+    {
+        'name': 'Daichi Kamada',
+        'value': '27',
+        'team': 'Lazio',
+    },
+]
