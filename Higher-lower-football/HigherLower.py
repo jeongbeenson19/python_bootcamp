0ca764bfb2ea016a65e89471b8b8a8f7f40193be
JeongbeenSon <jeongbeenson19@gmail.com>
2023-12-12 16:01:32 +0900

    231212

--- Higher-lower-football/HigherLower.py
+++ Higher-lower-football/HigherLower.py
@@ -3,58 +3,64 @@ import data
 import art
 import os
 
-player_profile = data.data
-game_end = False
+player_profiles = data.data
+game_over = False
 score = 0
-number = 14
+rounds = 14
 
-def clear():
+
+def clear_screen():
     os.system('clear')
-    
-#난수 생성
-def make_comparsion():
-    return player_profile[randint(0, len(player_profile) - 1)]
 
-#문제 출력
-first_comparison = make_comparsion()
 
-def HigherLower(first_comparison, score, game_end):
-    if game_end == True:
-        return
-    clear()
+def get_random_player():
+    return player_profiles[randint(0, len(player_profiles) - 1)]
+
+
+# Display a player's information
+def display_player(player):
+    print(f"{player['name']} who plays for {player['team']}")
+
+
+# Compare two players and prompt the user for a choice
+def make_comparison(player1, player2):
+    clear_screen()
     print(art.logo)
-    print(f"{first_comparison['name']} who play for {first_comparison['team']}")
+    display_player(player1)
     print(art.vs)
-    comparison = make_comparsion()
+    display_player(player2)
+    choice = input("Type '1' for the first player, '2' for the second player: ")
+    return choice
 
-    while comparison['value'] == first_comparison['value']:
-        comparison = make_comparsion()
 
-    print(f"{comparison['name']} who play for {comparison['team']}")
-    choice = input("Type '1' to choice first one, Type '2' to choice second one: ")
-    
-    if (choice == '1' and float(first_comparison['value']) > float(comparison['value'])) or (choice == '2' and float(comparison['value']) > float(first_comparison['value'])):
+# Main game loop
+def main_game(a):
+    global game_over, score
+
+    current_player = a
+    next_player = get_random_player()
+
+    while next_player['value'] == current_player['value']:
+        next_player = get_random_player()
+
+    user_choice = make_comparison(current_player, next_player)
+
+    if (user_choice == '1' and float(current_player['value']) > float(next_player['value'])) or (
+            user_choice == '2' and float(next_player['value']) > float(current_player['value'])):
         score += 1
+        current_player = next_player
+        clear_screen()
+        return current_player
     else:
-        print(f"you are wrong, you are final score is {score}")
-        print(f"{first_comparison['name']}'s value is {first_comparison['value']} \n{comparison['name']}'s value is {comparison['value']}")
-        game_end = True
-    
-    first_comparison = comparison
-
-    return first_comparison, score, game_end
-
-for n in range(0, number):
-    first_comparison, score, game_end = HigherLower(first_comparison, score, game_end)
-
-    if game_end == True:
-        break
+        print(f"You're wrong! Your final score is {score}")
+        print(
+            f"{current_player['name']}'s value is {current_player['value']}"
+            f"\n{next_player['name']}'s value is {next_player['value']}")
+        game_over = True
+        return game_over
 
 
-    
-
-#정답
-
-#채점
-
-#처리
+first_player = get_random_player()
+footballer = main_game(first_player)
+while not game_over:
+    footballer = main_game(footballer)
