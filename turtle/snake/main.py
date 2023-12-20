87ca5c04f72414d5cba239a736161d48146fef09
JeongbeenSon <jeongbeenson19@gmail.com>
2023-12-20 20:23:12 +0900

    231220

---
+++ turtle/snake/main.py
@@ -0,0 +1,25 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Snake game')
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(key='Up', fun=snake.up)
+screen.onkey(key='Down', fun=snake.down)
+screen.onkey(key='Right', fun=snake.right)
+screen.onkey(key='Left', fun=snake.left)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
