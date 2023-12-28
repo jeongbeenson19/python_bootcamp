7e72f77259cd09650a89ffb3637e2810fdb65e45
JeongbeenSon <jeongbeenson19@gmail.com>
2023-12-28 22:34:44 +0900

    231228

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,18 @@
+with open('./Input/Names/invited_names.txt', mode='r') as f:
+    names = f.readlines()
+
+n = 0
+for name in names:
+    names[n] = name.strip()
+    n += 1
+
+for name in names:
+    with open('./Input/Letters/starting_letter.txt', mode='r') as ff:
+        y = ff.read()
+        yy = y.replace("[name]", name)
+    with open(f'./Output/ReadyToSend/letter_for_{name}', mode='w') as fff:
+        fff.write(yy)
+        print(f'letter for {name} is saved successfully')
+
+print(names)
+
---
+++ day-25/main.py
@@ -0,0 +1,17 @@
+# with open('weather_data.csv', mode='r') as data:
+#     weather = data.readlines()
+#
+# print(weather)
+
+# import csv
+#
+# with open('weather_data.csv', mode='r') as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+#     print(type(temperatures[1]))
+
+import pandas
