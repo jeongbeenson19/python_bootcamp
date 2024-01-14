bd6e3851166780db755a95171217803cd8535d5f
JeongbeenSon <jeongbeenson19@gmail.com>
2024-01-14 18:17:10 +0900

    240114

--- kanye-quotes-start/main.py
+++ kanye-quotes-start/main.py
@@ -1,10 +1,14 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
+    response = requests.get(url="https://api.kanye.rest")
+    response.raise_for_status()
 
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfigure(quote_text, text=quote)
 
 
 window = Tk()
@@ -20,7 +24,6 @@ canvas.grid(row=0, column=0)
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
 
 
 window.mainloop()
