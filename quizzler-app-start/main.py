0944e6a2fd2f120816bbf06dd2f5e0c5cf758169
Jeongbeen_Son <jeongbeenson19@gmail.com>
2024-01-18 22:27:54 +0900

    240118

---
+++ quizzler-app-start/main.py
@@ -0,0 +1,24 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+import requests
+
+response = requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
