f0dc67e927b0373d27a8f5e6b37a17c8d861e934
JeongbeenSon <jeongbeenson19@gmail.com>
2024-02-03 21:01:15 +0900

    finished day45 lecture

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -1,6 +1,15 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html") as file:
-    content = file.read()
-
+response = requests.get(
+    url="https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+content = response.text
 soup = BeautifulSoup(content, "html.parser")
+
+movie_data = soup.find_all("h3", class_="title")
+
+movie_list = [movie.text for movie in movie_data]
+
+with open(file='bs4-start/The_100_Greatest_Movies.txt', mode='a') as file:
+    for n in range(1, len(movie_list)):
+        file.write(f"{movie_list[-n]}\n")
